f2514242c92f72c21348eb082ec9b2eaa25db99b
Jose Carlos <jose.ramirez@inei.gob.pe>
2018-06-11 21:18:31 -0500

    integrando estructura para cuadros

--- src/js/events/cuadros.js
+++ src/js/events/cuadros.js
@@ -1,5 +1,6 @@
-(function (utils) {
-      $(document).ready(function (e) {
-
-      });
-})(App.utils);
+(function (service, utils) {
+    $(document).ready(function (e) {
+        //utils.cuadros.nacional();
+        utils.cuadros.cabeceraUigeos(['00', '15', '11', '16', '18', '25', '25'])
+    });
+})(App.service, App.utils);
--- src/js/utils/cuadros.js
+++ src/js/utils/cuadros.js
@@ -1,3 +1,68 @@
-App.utils.cuadros = (function (parent, config) {
+App.utils.cuadros = (function (parent, service, config) {
+    var crearCabecera = function (cabecera) {
+        var thead = '<tr>';
+        var childrens = [];
+        for (var i=0; i<cabecera.length;i++) {
+            var v = cabecera[i];
+            var rowspan = (v.rowspan !== undefined) ? ' rowspan="'+v.rowspan+'"' : '';
+            var colspan = (v.colspan !== undefined) ? ' colspan="'+v.colspan+'"' : '';
+            var th = '<th'+rowspan+colspan+'>'+v.titulo+'</th>';
+            thead += th;
 
-})(App, AppConfig());
+            if (v.children !== undefined) {
+                console.log("zquiii", v.children);
+                childrens = childrens.concat(v.children);
+            }
+        }
+        thead += '</tr>';
+
+        if (childrens.length > 0) {
+            thead += '<tr>';
+            for (var j=0; j<childrens.length;j++) {
+                var k = childrens[j];
+                thead += '<th>'+k.titulo+'</th>'
+            }
+            thead += '</tr>';
+        }
+
+        $("#tblindicadores thead").html(thead);
+
+        service.cuadros.getNacional(function (data) {
+
+        });
+    };
+
+    var cabecera = [
+        {
+            "titulo": "VARIABLE / INDICADOR",
+            "rowspan": "2"
+        }
+    ];
+
+    var cabeceraTemplate = {
+        "titulo": "{0}",
+        "colspan": "2",
+        "children": [
+            {
+                "titulo": "Absoluto"
+            },
+
+            {
+                "titulo": "%"
+            }
+        ]
+    };
+
+    var cabeceraUigeos = function (ubigeos) {
+        for (var i=0;i<ubigeos.length;i++) {
+            cabeceraTemplate.titulo = "Nombre";
+            cabecera.push(cabeceraTemplate);
+        }
+
+        crearCabecera(cabecera);
+    };
+    return {
+        cabeceraUigeos: cabeceraUigeos,
+        nacional: undefined
+    }
+})(App.utils, App.service, AppConfig());
