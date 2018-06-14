c8d7561b34ba3e43f1863ac42fe3dcac0537341d
fsoto <franksoto2012@gmail.com>
2018-06-13 20:17:39 -0500

    integracion con servicio de legenda

--- src/js/config_example.js
+++ src/js/config_example.js
@@ -1,9 +1,12 @@
 var AppConfig = function () {
     return {
-        urlServer: "http://127.0.0.1:8001/",
+        urlServer: "http://127.0.0.1:8045/",
+
         map_config:
-            {poblacion: { urlMap:"http://192.168.202.86:6080/arcgis/rest/services/RESULTADO_CPV/RESULTADO_POBLACION_1/MapServer" , cod_tematico_default:"P010100" }}
+            { 'POB': { urlMap:"http://192.168.202.86:6080/arcgis/rest/services/RESULTADO_CPV/RESULTADO_POBLACION_1/MapServer" , cod_tematico_default:"P010100"  }},
 
+        utils:{
+            print:"http://192.168.202.84:6080/arcgis/rest/services/Utilities/PrintingTools/GPServer/Export%20Web%20Map%20Task"
+        }
     }
-
 };
